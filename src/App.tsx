2abafa3e705096539e878e0e475681a5295a1ea4
umezawakanta <k.umezawa@mighty-link.com>
2024-09-26 07:54:17 +0900

    Themeの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import WorkTimeEntryForm from "./components/forms/WorkTimeEntryForm";
@@ -7,22 +8,39 @@ import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
 // import PrivateRoute from "./components/PrivateRoute";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1976d2",
+    },
+    secondary: {
+      main: "#dc004e",
+    },
+  },
+  typography: {
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+  },
+});
+
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/work-time" element={<WorkTimeEntryForm />} />
-        <Route path="/reports" element={<Reports />} />
-        {/* <Route element={<PrivateRoute />}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/work-time" element={<WorkTimeEntryForm />} />
           <Route path="/reports" element={<Reports />} />
-        </Route> */}
-        <Route path="/404" element={<NotFound />} />
-        <Route path="*" element={<Navigate to="/404" replace />} />
-      </Routes>
-    </Layout>
+          {/* <Route element={<PrivateRoute />}>
+            <Route path="/work-time" element={<WorkTimeEntryForm />} />
+            <Route path="/reports" element={<Reports />} />
+          </Route> */}
+          <Route path="/404" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/404" replace />} />
+        </Routes>
+      </Layout>
+    </ThemeProvider>
   );
 }
 
