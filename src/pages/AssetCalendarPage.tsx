5963d9bb26db5da0375e3ca1d2125359982f704e
umezawakanta <k.umezawa@mighty-link.com>
2024-10-04 00:56:59 +0900

    更新したらデータが消える件

--- src/pages/AssetCalendarPage.tsx
+++ src/pages/AssetCalendarPage.tsx
@@ -1,31 +1,38 @@
-import { useSelector } from "react-redux";
-import { RootState } from "@/store";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState, AppDispatch } from "@/store";
 import { AssetCalendar } from "@/components/calender/AssetCalender";
-import { AssetEntry } from "@/store/assetSlice";
-import { DebtEntry } from "@/store/debtSlice";
-
-interface DataPoint {
-  date: Date;
-  value: number;
-  account: string;
-}
+import { fetchAssetEntries } from "@/store/assetSlice";
+import { fetchDebtEntries } from "@/store/debtSlice";
 
 export function AssetCalendarPage() {
+  const dispatch = useDispatch<AppDispatch>();
   const assetEntries = useSelector((state: RootState) => state.asset.entries);
   const debtEntries = useSelector((state: RootState) => state.debt.entries);
+  const assetStatus = useSelector((state: RootState) => state.asset.status);
+  const debtStatus = useSelector((state: RootState) => state.debt.status);
 
-  const combinedData: DataPoint[] = [
-    ...assetEntries.map((entry: AssetEntry) => ({
+  useEffect(() => {
+    if (assetStatus === "idle") {
+      dispatch(fetchAssetEntries());
+    }
+    if (debtStatus === "idle") {
+      dispatch(fetchDebtEntries());
+    }
+  }, [dispatch, assetStatus, debtStatus]);
+
+  const combinedData = [
+    ...assetEntries.map((entry) => ({ ...entry, date: new Date(entry.date) })),
+    ...debtEntries.map((entry) => ({
+      ...entry,
       date: new Date(entry.date),
-      value: entry.value,
-      account: entry.account,
-    })),
-    ...debtEntries.map((entry: DebtEntry) => ({
-      date: new Date(entry.date),
-      value: -entry.value, // Negate debt values
-      account: entry.account,
+      value: -entry.value,
     })),
   ];
+
+  if (assetStatus === "loading" || debtStatus === "loading") {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
