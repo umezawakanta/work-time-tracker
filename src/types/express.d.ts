a7d2941cd92ac0ba5cb49695e291888d0c472156
umezawakanta <k.umezawa@mighty-link.com>
2025-03-12 20:06:17 +0900

    エラー修正

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,13 +1,20 @@
 import 'express';
 
-declare module 'express' {
-  interface Request {
-    user?: any;
-  }
-
-  interface Response {
-    // Express 5の型に合わせて明示的にメソッドを定義
-    json(body?: any): this;
-    status(code: number): this;
+declare global {
+  namespace Express {
+    interface Request {
+      user?: any;
+    }
   }
 }
+
+declare module 'express' {
+  interface Request {
+      body: any;
+      params: any;
+      query: any;
+      headers: any;
+  }
+}
+
+export {};
