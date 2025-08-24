169cfb5d22cdef3a8f74b805c1995c334fc242bd
kanta13jp1 <kanta13jp@gmail.com>
2025-08-25 01:56:37 +0900

    fix: update Vite configuration to use the official React plugin and include 'react/jsx-runtime' for improved compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 ﻿import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import path from 'path';
 import { VitePWA } from 'vite-plugin-pwa';
 import { visualizer } from 'rollup-plugin-visualizer';
@@ -316,6 +316,7 @@ export default defineConfig(({ command, mode }) => {
       include: [
         'react',
         'react-dom',
+        'react/jsx-runtime',
         'react-router-dom',
         'react-is',
         'prop-types',
