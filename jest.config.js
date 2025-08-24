0cbd68f216a3b3c17e632eae5b910a3b75d7a10e
kanta13jp1 <kanta13jp@gmail.com>
2025-08-24 23:28:53 +0900

    テスト

--- jest.config.js
+++ jest.config.js
@@ -19,9 +19,9 @@ module.exports = {
     '^@/services/auth/TokenManager$': '<rootDir>/src/__mocks__/TokenManager.js',
     // CSS files specific handling
     '^@/styles/(.*)\\.(css|scss|sass|less)$': 'identity-obj-proxy',
+    '^@/lib/track$': '<rootDir>/src/__mocks__/track.ts',
     // General path aliases after specific mappings
     '^@/(.*)$': '<rootDir>/src/$1',
-    '^@/lib/track$': '<rootDir>/src/__mocks__/track.ts',
     // Asset mocks - all CSS files
     '\\.(css|less|scss|sass|styl)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/__mocks__/fileMock.js',
