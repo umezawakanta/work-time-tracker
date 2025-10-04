4f532997c89419cd44976d209efa5f0c0b7327e1
kanta13jp1 <kanta13jp1@users.noreply.github.com>
2025-10-04 21:02:35 +0900

    モデル定義の一貫性を向上させるため、Mongooseモデルの取得方法を修正しました。これにより、コードの可読性が向上しました。

--- api/utils/schemas.ts
+++ api/utils/schemas.ts
@@ -233,13 +233,13 @@ TimeEntrySchema.pre('save', function(next) {
 });
 
 // Create models
-const User = mongoose.models.User || mongoose.model('User', UserSchema);
-const Book = mongoose.models.Book || mongoose.model('Book', BookSchema);
-const WorkRecord = mongoose.models.WorkRecord || mongoose.model('WorkRecord', WorkRecordSchema);
-const Memo = mongoose.models.Memo || mongoose.model('Memo', MemoSchema);
-const UserSettings = mongoose.models.UserSettings || mongoose.model('UserSettings', UserSettingsSchema);
-const Project = mongoose.models.Project || mongoose.model('Project', ProjectSchema);
-const TimeEntry = mongoose.models.TimeEntry || mongoose.model('TimeEntry', TimeEntrySchema);
+const User = mongoose.models['User'] || mongoose.model('User', UserSchema);
+const Book = mongoose.models['Book'] || mongoose.model('Book', BookSchema);
+const WorkRecord = mongoose.models['WorkRecord'] || mongoose.model('WorkRecord', WorkRecordSchema);
+const Memo = mongoose.models['Memo'] || mongoose.model('Memo', MemoSchema);
+const UserSettings = mongoose.models['UserSettings'] || mongoose.model('UserSettings', UserSettingsSchema);
+const Project = mongoose.models['Project'] || mongoose.model('Project', ProjectSchema);
+const TimeEntry = mongoose.models['TimeEntry'] || mongoose.model('TimeEntry', TimeEntrySchema);
 
 // Debug logging
 console.log('Memo model created:', !!Memo);
